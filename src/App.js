3dcf81b6a1ab4561c418367039db518009d4605b
albertcai101 <albertcai.paloalto@gmail.com>
2024-05-16 03:38:51 -0700

    better arrangement

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import WaitlistWidget from './WaitlistWidget';
 
 function App() {
   return (
     <div className="App">
-      <WaitlistWidget/>
+      <header className="App-header">
+        <h1 className="title">spellr</h1>
+        <p className="description">Individually-paced digital phonics education for early English education.</p>
+        <div className="widget-container">
+          <WaitlistWidget />
+        </div>
+      </header>
     </div>
   );
 }
